61e0a8fef969a588445071f06b51955e83a5f6e9
Sylvia van Os <sylvia.avans@gmail.com>
2017-05-30 12:27:10 +0200

    Push WIP

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../game';
 import { GameService } from '../game.service';
+import { Tile } from '../tile';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,12 @@ import { GameService } from '../game.service';
 })
 export class DashboardComponent implements OnInit {
   games: Game[] = [];
+  tiles: Tile[] = [];
   constructor(private gameService: GameService) { }
 
   ngOnInit() {
     this.gameService.getGames().then(games => this.games = games);
+    this.gameService.getTiles('592d396d8f889900111e694a').then(tiles => this.tiles = tiles);
   }
 
 }
--- src/app/tile.ts
+++ src/app/tile.ts
@@ -2,4 +2,16 @@ export class Tile {
     xPos: number;
     yPos: number;
     zPos: number;
+    tile: {
+        id: number;
+        suit: string;
+        name: number;
+        matchesWholeSuit: boolean;
+    };
+    _id: string;
+    match: {
+        foundBy: string;
+        otherTileId: string;
+        foundOn: Date;
+    };
 }
