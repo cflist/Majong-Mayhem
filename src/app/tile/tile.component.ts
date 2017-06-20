d41d677baf7af1f0e06a54decf10bbde498f6437
Sylvia van Os <sylvia.avans@gmail.com>
2017-06-20 12:26:49 +0200

    FIX OFF BY ONE OMFG

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -18,13 +18,13 @@ export class TileComponent {
   constructor() { }
 
   public getTileStyle() {
-    var x = 57;
-    var y = 79;
+    var width = 60;
+    var height = 82.5;
     var style = {
       //'left': ((this.tile.xPos * 30) + (this.tile.zPos * 5)) + "px",
       //'top': ((this.tile.yPos * 30) + (this.tile.zPos * 5)) + "px",
-      'left': (this.tile.xPos * (x/2)) + (this.tile.zPos * 5) + "px",
-      'top': (this.tile.yPos * (y/2)) - (this.tile.zPos * 5) + "px",
+      'top': (this.tile.yPos * (height/2)) - (this.tile.zPos * 5) + "px",
+      'left': (this.tile.xPos * (width/2)) + (this.tile.zPos * 5) + "px",
       'z-index': this.tile.zPos
     }
 
@@ -34,11 +34,14 @@ export class TileComponent {
   public getTileUnicode() {
     switch(this.tile.tile.suit) {
       case "Bamboo":
-        return 'zxcvbnm,.'.charAt(+this.tile.tile.name);
+        if (+this.tile.tile.name > 9) break;
+        return 'zxcvbnm,.'.charAt(+this.tile.tile.name - 1);
       case "Character":
-        return 'qwertyuio'.charAt(+this.tile.tile.name);
+        if (+this.tile.tile.name > 9) break;
+        return 'qwertyuio'.charAt(+this.tile.tile.name - 1);
       case "Circle":
-        return 'asdfghjkl'.charAt(+this.tile.tile.name);
+        if (+this.tile.tile.name > 9) break;
+        return 'asdfghjkl'.charAt(+this.tile.tile.name - 1);
       case "Dragon":
         switch(this.tile.tile.name) {
           case "Red": return 7;
@@ -77,44 +80,4 @@ export class TileComponent {
     // Error
     return 'A';
   }
-
-//    public selectTile(tile) {
-//      if (this.selectedTiles.length == 1) {
-//        if (tile == this.selectedTiles[0]) {
-//          tile.selected = false;
-//          this.selectedTiles = [];
-//          return;
-//        }
-//      }
-//
-//      this.selectedTiles.push(tile);
-//      tile.selected = true;
-//      if (this.selectedTiles.length == 2)
-//        this.tileMatches();
-//    }
-//
-//    public tileMatches() {
-//      var tiles = this.selectedTiles;
-//      this.selectedTiles = [];
-//
-//      tiles[0].selected = false;
-//      tiles[1].selected = false;
-//
-//      console.log(tiles);
-//
-//      if (tiles[0].tile.suit == tiles[1].tile.suit) {
-//        if (tiles[0].matchesWholeSuit ||
-//            (tiles[0].tile.name == tiles[1].tile.name)) {
-//              this.gameService.postTileMatch(this.gameId, tiles).then(object => {
-//                if (object.message){
-//                alert(object.message.replace(/{{.*}} /g, ''));}
-//              });
-//              return;
-//        } else {
-//          alert("The chosen tiles are not a match");
-//        }
-//      } else {
-//        alert("The chosen tiles are not of the same suit");
-//      }
-//    }
 }
